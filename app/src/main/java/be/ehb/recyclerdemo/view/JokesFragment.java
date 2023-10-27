9307e3c4b64dcf2028f8162566c7089c3d5d0c63
David-VS <63110701+David-VS@users.noreply.github.com>
2023-10-27 11:24:08 +0200

    added dummydata

--- app/src/main/java/be/ehb/recyclerdemo/view/JokesFragment.java
+++ app/src/main/java/be/ehb/recyclerdemo/view/JokesFragment.java
@@ -10,6 +10,7 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
@@ -43,12 +44,18 @@ public class JokesFragment extends Fragment {
 
         JokesViewModel mJokesViewModel = new ViewModelProvider(getActivity()).get(JokesViewModel.class);
         JokeRowAdapter mJokeRowAdapter = new JokeRowAdapter();
+        //RecyclerView.LayoutManager mLayoutManager= new GridLayoutManager(getContext(), 2);
         RecyclerView.LayoutManager mLayoutManager= new LinearLayoutManager(getContext(), RecyclerView.VERTICAL, false);
 
         jokesRV.setAdapter(mJokeRowAdapter);
         jokesRV.setLayoutManager(mLayoutManager);
 
-        mJokesViewModel.getLiveJokes().observe(getViewLifecycleOwner(), jokes -> mJokeRowAdapter.newItems(jokes));
+        mJokesViewModel.getLiveJokes().observe(getViewLifecycleOwner(), new Observer<ArrayList<Joke>>() {
+            @Override
+            public void onChanged(ArrayList<Joke> jokes) {
+                mJokeRowAdapter.newItems(jokes);
+            }
+        });
 
     }
 }
--- app/src/main/java/be/ehb/recyclerdemo/viewmodel/JokesViewModel.java
+++ app/src/main/java/be/ehb/recyclerdemo/viewmodel/JokesViewModel.java
@@ -25,6 +25,11 @@ public class JokesViewModel extends AndroidViewModel {
         jokes.add(new Joke("Het is grijs en als het in uw oog vliegt ben je dood", "Een boeing"));
         jokes.add(new Joke("het is oranje en als het regent verdwijnt het", "de mannen van de gemeente"));
         jokes.add(new Joke("Het is wit en het staat in de hoek", "een stoute frigo"));
+        jokes.add(new Joke("Het is blauw en weegt niet veel", "lichtblauw"));
+        jokes.add(new Joke("odysee is ook een toffe school", "<mic drop>"));
+        jokes.add(new Joke("het is groen en glijd van een berg", "een slawine"));
+        jokes.add(new Joke("het is groen en glijdt van een berg", "een skiwi"));
+        jokes.add(new Joke("Hoeveel programmeurs heb je nodig om een lamp te vervangen", "geen, dat is een hardwareprobleem"));
 
         liveJokes.setValue(jokes);
     }
